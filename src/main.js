4e05c7c17569f34d5ffea449512138e22576e45a
Victor Lemos <victor.lemos@outlook.com>
2020-01-28 00:20:08 -0300

    Estruturação de pastas e Adição de PréProcessador

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
-import vuetify from './plugins/vuetify';
+import vuetify from "@/plugins/vuetify";
 
 Vue.config.productionTip = false
-
-Vue.use(Vuetify);
 
 new Vue({
   vuetify,
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
+const path = require('path')
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: []
+    }
+  }
+}
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/*.scss'),
+      ],
+    })
 }
