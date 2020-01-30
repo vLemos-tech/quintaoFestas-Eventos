54e0ed7fccc520fa7fe8464fa7e95187ec7a3e6c
Lucas Estrela <lucaspaimaro.le@gmail.com>
2020-01-30 00:58:23 -0300

    styling the component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"> 
     <NavBar />
-    <Swiper />
+      <Swiper />
   </v-app>
 </template>
 
--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,12 +1,14 @@
 <template>
-  <md-card>
+  <md-card class="back" style="height:auto">
     <md-card-media class="swiper-inner">
       <!-- swiper -->
       <swiper :options="swiperOption">
-        <swiper-slide v-for="foto in fotos" :key="foto.name">
-            <v-img :src="`${foto.url}`"></v-img>
+        <swiper-slide v-for="foto in fotos" :key="foto.name" >
+            <v-img :src="`${foto.url}`" height="100%" width="100%"></v-img>
         </swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
       </swiper>
     </md-card-media>
   </md-card>
@@ -20,16 +22,37 @@
           effect: 'coverflow',
           grabCursor: true,
           centeredSlides: true,
-          slidesPerView: "auto",
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+          speed:800,
+          slidesPerView: '2',
+          loop:true,
           coverflowEffect: {
             rotate: 50,
-            stretch: 0,
+            stretch: 150,
             depth: 100,
             modifier: 1,
-            slideShadows : true
+            slideShadows : false
           },
           pagination: {
             el: '.swiper-pagination'
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          breakpoints: {
+            1023: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            }
+          },
+          on: {
+            imagesReady: function(){
+              this.el.classList.remove('loading');
+            }
           }
         },
         fotos: [],
@@ -42,65 +65,59 @@
         createFotos(){
             this.fotos = [
                 {
-                    "name": "Caraio Mary Jaine",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "1",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "So Mauzaum",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",                
+                    "name": "2",
+                    "url": "https://images3.alphacoders.com/975/975999.png",                
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "3",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Olha o bixo vinu",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "4",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "4",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "6",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "7",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "8",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
+                },
+
+                {
+                    "name": "9",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "10",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "11",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "12",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
                 {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
+                    "name": "13",
+                    "url": "https://images3.alphacoders.com/975/975999.png",
                 },
-                {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
-                },
-                {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
-                },
-                {
-                    "name": "Partiu Suruba",
-                    "url": "https://pm1.narvii.com/6287/eb74d1b223d472d095d51f17c90f3b4349e565ca_hq.jpg",
-                },
+
             ];
 
             //this.fotos = this.fotos.slice(0,5);
@@ -109,17 +126,146 @@
   }
 </script>
 
-<style scoped>
-  .swiper-inner {
+<style scoped lang="scss">
+.back{
+    background-color: #FFC3C2
+}
+
+%transition_all_03s {
+  transition:all .3s ease;
+}
+%backface_visibility_hidden{
+  backface-visibility:hidden;
+  -webkit-backface-visibility:hidden;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding-bottom: 5%;
+}
+
+.swiper-container {
+  width: 100%;
+  height: 100%;
+  transition:opacity .6s ease;
+  &.swiper-container-coverflow{
+    padding-top:5%;
+  }
+  &.loading{
+    opacity:0;
+    visibility:hidden;
+  }
+  &:hover{
+    .swiper-button-prev,
+    .swiper-button-next{
+      transform:translateX(0);
+      opacity:1;
+      visibility:visible;
+    }
+  }
+}
+.swiper-wrapper{
+
+}
+.swiper-slide{
+  background-position:center;
+  background-size:cover;
+    height: 864px;
+  .slide-inner{
     width: 100%;
-    height: 400px;
+    height: 710px;
     padding-top: 50px;
     padding-bottom: 50px;
   }
-  .swiper-slide {
-    background-position: center;
-    background-size: cover;
-    width: 300px;
-    height: 300px;
+  .entity-img{
+    display:none;
   }
+  .content{
+    position:absolute;
+    top:40%;
+    left:0;
+    width:50%;
+    padding-left:5%;
+    color:#fff;
+    .title{
+      font-size:2.6em;
+      font-weight:bold;
+      margin-bottom:30px;
+    }
+    .caption{
+      display:block;
+      font-size:13px;
+      line-height:1.4;
+    }
+  }
+}
+[class^="swiper-button-"]{
+  width:44px;
+  opacity:0;
+  visibility:hidden;
+  @extend %transition_all_03s;
+}
+.swiper-button-prev{
+  transform:translateX(50px);
+}
+.swiper-button-next{
+  transform:translateX(-50px);
+}
+.swiper-container-horizontal{
+  >.swiper-pagination-bullets{
+    .swiper-pagination-bullet{
+      margin:0 9px;
+      position:relative;
+      width:12px;
+      height:12px;
+      background-color:#fff;
+      opacity:.4;
+      @extend %transition_all_03s;
+      &::before{
+        content:'';
+        position:absolute;
+        top:50%;
+        left:50%;
+        width:18px;
+        height:18px;
+        transform:translate(-50%, -50%);
+        border:0px solid #fff;
+        border-radius:50%;
+        @extend %transition_all_03s;
+      }
+      &:hover,
+      &.swiper-pagination-bullet-active{
+        opacity:1;
+      }
+      &.swiper-pagination-bullet-active{
+        &::before{
+          border-width:1px;
+        }
+      }
+    }
+  }
+}
+
+// Mediaqueries
+@media(max-width:1180px){
+  .swiper-slide{
+    .content{
+      .title{
+        font-size:25px;
+      }
+      .caption{
+        font-size:12px;
+      }
+    }
+  }
+}
+@media(max-width:1023px){
+  .swiper-container {
+    height:40vw;
+    &.swiper-container-coverflow{
+      padding-top:0;
+    }
+  }
+}
 </style>
